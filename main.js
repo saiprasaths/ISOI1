22e57110bd05e2bbdbc3a80085191d4f2dee98c8
hari <harirohith123@gmail.com>
2021-07-22 11:43:21 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,25 @@
+!(function($) {
+    "use strict";
+
+    // Toggle .header-scrolled class to #header when page is scrolled
+   
+
+    // Back to top button
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 100) {
+            $('.gotopbtn').fadeIn('slow');
+        } else {
+            $('.gotopbtn').fadeOut('slow');
+        }
+    });
+
+    $('.gotopbtn').click(function() {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 500, 'easeInOutExpo');
+        return false;
+    });
+
+    
+
+})(jQuery);
